e58dc25f4fce507b4319b4ad40e08123727b57a4
emiliocm9 <emilio.contreras97@gmail.com>
2021-02-06 01:38:20 -0600

    docs: validation in registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, length: { in: 5..6 }, uniqueness: true
+  validates :name, format: {with: /\A[^0-9`!@#\$%\^&*+_=]+\z/, message:"no debe estar vacío ni incluir caracteres especiales."} # required
+  validates :phone, format: {with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/, message:"no debe estar vacío ni incluir letras."},
+  length: {maximum: 10, message: "debe de ser de 10 digitos máximo"} # required
+  validates :status, presence: true # required
 end

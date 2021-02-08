48d6648cfe59e6aae994ca207dd043d507551560
emiliocm9 <emilio.contreras97@gmail.com>
2021-02-08 10:56:48 -0600

    docs: sign and table styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, length: { in: 5..6 }, uniqueness: true
+  validates :username, length: { in: 5..10 }, uniqueness: true
   validates :name, format: {with: /\A[^0-9`!@#\$%\^&*+_=]+\z/, message:"no debe estar vacío ni incluir caracteres especiales."} # required
   validates :phone, format: {with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/, message:"no debe estar vacío ni incluir letras."},
   length: {maximum: 10, message: "debe de ser de 10 digitos máximo"} # required
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'home/index'
+  get 'home/signedin'
+  devise_for :users,  controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    root to: "devise/registrations#new"
-  end
+  root to: "home#index"
 end

0e4d54c917bcbb50e123097de38136c4bc5d8e23
emiliocm9 <emilio.contreras97@gmail.com>
2021-02-18 21:07:57 -0600

    recovering from the git fiasco

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'La sesión se ha cerrado'
+    redirect_to home_path
   end
 end

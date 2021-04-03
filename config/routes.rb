57c55c79dd23c31c670c54cfb00efb04d790a30f
emiliocm9 <emilio.contreras97@gmail.com>
2021-04-02 22:46:16 -0600

    docs: restart puma skip

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
-  root to: 'home'
+  root to: "sessions#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
